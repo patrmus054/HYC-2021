964ccd6a53e6da3e3092fa178aaa06a36c755de9
kasiapetka <katarzynapetka7@gmail.com>
2021-05-14 15:40:46 +0200

    blank messages fixeed

--- chat/src/ChatRoom/ChatRoom.js
+++ chat/src/ChatRoom/ChatRoom.js
@@ -39,7 +39,7 @@ const ChatRoom = (props) => {
   };
 
   const handleKeyPressed = (event) => {
-    if (event.key === "Enter" && ewMessage.trim().length !== 0) {
+    if (event.key === "Enter" && newMessage.trim().length !== 0) {
       setPressed(true);
       handleSendMessage();
       setTimeout(() => {
